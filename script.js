a7924a50a09dbd71695ae48a0339e9a9716a21d2
JonathanSAndrade <jonathan.andrade9.souza@hotmail.com>
2025-07-22 22:12:20 -0300

    Update JS

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function generateNumber() {
- const min = Math.ceil(document.querySelector('.input-min').value)
- const max = Math.floor(document.querySelector('.input-max').value)
- 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
 
-    alert(`O número sorteado é: ${result}`);
- 
+    if (min >= max) {
+        alert('O valor mínimo tem que ser menor ao valor máximo.');
+    }   else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
+        alert(`O número sorteado é: ${result}`);
+    }
 }
